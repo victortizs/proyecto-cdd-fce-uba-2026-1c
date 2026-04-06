e46ed0c99f56da75de4d918333e7bc9f64b9aaf0
victortizs <victortizs@outlook.com>
2026-04-06 12:45:09 -0300

    script de prueba para ver la base principal y filtrar empresas

--- utils/prueba_filtro_empresa.R
+++ utils/prueba_filtro_empresa.R
@@ -1,7 +1,7 @@
 library(readr)
 library(dplyr)
 
-df = read_csv(r"(C:\Users\victo\Downloads\pozos-en-perforacin.csv)")
+df = read_csv(r"(http://datos.energia.gob.ar/dataset/7ea2ac77-d7a0-4129-9fbf-6f1a25d94e21/resource/7fcd6c41-9358-4e16-96f1-0380444bff26/download/pozos-en-perforacin-por-empresa.csv)")
 glimpse(df)
 
 cat("Valores únicos por columna:")
